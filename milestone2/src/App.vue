b0b0f708341e84e368e2fea1b2b33add519b12d8
FabioLaForesta <fabiolaforesta@icloud.com>
2022-05-24 17:43:54 +0200

    Adding Milestone 2

--- milestone2/src/App.vue
+++ milestone2/src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="all">
+    <header id="site_header">
+      <div class="Upbarr">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+          <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+          <path
+            d="M248 8C111.1 8 0 119.1 0 256s111.1 248 248 248 248-111.1 248-248S384.9 8 248 8zm100.7 364.9c-4.2 0-6.8-1.3-10.7-3.6-62.4-37.6-135-39.2-206.7-24.5-3.9 1-9 2.6-11.9 2.6-9.7 0-15.8-7.7-15.8-15.8 0-10.3 6.1-15.2 13.6-16.8 81.9-18.1 165.6-16.5 237 26.2 6.1 3.9 9.7 7.4 9.7 16.5s-7.1 15.4-15.2 15.4zm26.9-65.6c-5.2 0-8.7-2.3-12.3-4.2-62.5-37-155.7-51.9-238.6-29.4-4.8 1.3-7.4 2.6-11.9 2.6-10.7 0-19.4-8.7-19.4-19.4s5.2-17.8 15.5-20.7c27.8-7.8 56.2-13.6 97.8-13.6 64.9 0 127.6 16.1 177 45.5 8.1 4.8 11.3 11 11.3 19.7-.1 10.8-8.5 19.5-19.4 19.5zm31-76.2c-5.2 0-8.4-1.3-12.9-3.9-71.2-42.5-198.5-52.7-280.9-29.7-3.6 1-8.1 2.6-12.9 2.6-13.2 0-23.3-10.3-23.3-23.6 0-13.6 8.4-21.3 17.4-23.9 35.2-10.3 74.6-15.2 117.5-15.2 73 0 149.5 15.2 205.4 47.8 7.8 4.5 12.9 10.7 12.9 22.6 0 13.6-11 23.3-23.2 23.3z"
+          />
+        </svg>
+      </div>
+    </header>
+    <!-- /#site_header -->
+
+    <main id="site_main">
+      <section id="music" class="my-5">
+        <div class="container">
+          <div class="row row-cols-5 g-5">
+            <div class="col">
+              <div class="card">
+                <img src="<?= $album['poster'] ?>" alt="" />
+                <h4 class="text-center text-light">title</h4>
+                <h5 class="text-center c_text_grey">author</h5>
+                <h5 class="text-center c_text_grey">year</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <!-- /#site_main -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"; // - Importo Axios
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data(){
+    return{
+      Api_link: 'http://localhost/PHP/php-ajax-dischi/milestone1/db.php'
+    }
+  },
+  methods: {
+    callApi() {
+      axios
+        .get(this.Api_link)
+        .then((response) => {
+          console.log(response);
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/assets/scss/style.scss"; // - Import style.scss
 </style>
